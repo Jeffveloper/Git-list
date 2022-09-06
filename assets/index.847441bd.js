4611e80d5d487d9fc4d7b55cfd8eb74b6843d8e7
Jefferson <jeffersonaraujo491@gmail.com>
2022-09-06 00:26:13 -0500

    Updates

--- assets/index.847441bd.js
+++ assets/index.847441bd.js
@@ -337,9 +337,9 @@ Error generating stack: `+o.message+`
 		font-family: "Inter";
 		font-style: normal;
 		font-weight: 300;
-		src: local(""), url("/fonts/inter-v12-latin-300.woff2") format("woff2"),
+		src: local(""), url("./fonts/inter-v12-latin-300.woff2") format("woff2"),
 			/* Chrome 26+, Opera 23+, Firefox 39+ */
-				url("/fonts/inter-v12-latin-300.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+				url("./fonts/inter-v12-latin-300.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 	}
 
 	/* inter-regular - latin */
@@ -347,9 +347,9 @@ Error generating stack: `+o.message+`
 		font-family: "Inter";
 		font-style: normal;
 		font-weight: 400;
-		src: local(""), url("/fonts/inter-v12-latin-regular.woff2") format("woff2"),
+		src: local(""), url("./fonts/inter-v12-latin-regular.woff2") format("woff2"),
 			/* Chrome 26+, Opera 23+, Firefox 39+ */
-				url("/fonts/inter-v12-latin-regular.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+				url("./fonts/inter-v12-latin-regular.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 	}
 
 	/* inter-500 - latin */
@@ -357,9 +357,9 @@ Error generating stack: `+o.message+`
 		font-family: "Inter";
 		font-style: normal;
 		font-weight: 500;
-		src: local(""), url("/fonts/inter-v12-latin-500.woff2") format("woff2"),
+		src: local(""), url("./fonts/inter-v12-latin-500.woff2") format("woff2"),
 			/* Chrome 26+, Opera 23+, Firefox 39+ */
-				url("/fonts/inter-v12-latin-500.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+				url("./fonts/inter-v12-latin-500.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 	}
 
 	/* inter-600 - latin */
@@ -367,9 +367,9 @@ Error generating stack: `+o.message+`
 		font-family: "Inter";
 		font-style: normal;
 		font-weight: 600;
-		src: local(""), url("/fonts/inter-v12-latin-600.woff2") format("woff2"),
+		src: local(""), url("./fonts/inter-v12-latin-600.woff2") format("woff2"),
 			/* Chrome 26+, Opera 23+, Firefox 39+ */
-				url("/fonts/inter-v12-latin-600.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+				url("./fonts/inter-v12-latin-600.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 	}
 
 	/* inter-700 - latin */
@@ -377,9 +377,9 @@ Error generating stack: `+o.message+`
 		font-family: "Inter";
 		font-style: normal;
 		font-weight: 700;
-		src: local(""), url("/fonts/inter-v12-latin-700.woff2") format("woff2"),
+		src: local(""), url("./fonts/inter-v12-latin-700.woff2") format("woff2"),
 			/* Chrome 26+, Opera 23+, Firefox 39+ */
-				url("/fonts/inter-v12-latin-700.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+				url("./fonts/inter-v12-latin-700.woff") format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 	}
 `,Xh=ah`
   ${Kh}
